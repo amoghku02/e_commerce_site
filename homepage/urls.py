6a0bd32be126e95ff834a328cf77815b745f9991
Amogh Kumar <amoghku02@gmail.com>
2020-10-16 13:42:25 +0530

    initial changes

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('help/about-us', views.about_us, name='about-us'),
+    path('careers', views.careers, name='careers'),
+    path('stories', views.stories, name='stories'),
+    path('help/return-policy', views.return_policy, name='return-policy'),
+    path('help/terms-of-use', views.terms_of_use, name='terms-of-use'),
+    path('help/privacy', views.privacy, name='privacy')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'homepage/home.html')
+
+def about_us(request):
+    return render(request, 'footer/about_us.html')
+
+def careers(request):
+    return render(request, 'footer/careers.html')
+
+def stories(request):
+    return render(request, 'footer/stories.html')
+
+def return_policy(request):
+    return render(request, 'footer/return_policy.html')
+
+def terms_of_use(request):
+    return render(request, 'footer/terms_of_use.html')
+
+def privacy(request):
+    return render(request, 'footer/privacy.html')
